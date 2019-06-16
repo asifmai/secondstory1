eeee00dd9ba4ee21e845b47f3463b60f75e12282
iqbal <asifmai@hotmail.com>
2019-06-16 10:56:52 +0500

    done

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const fs = require('fs');
 (async () => {
   try {
     console.log('Started Scraping at: ', new Date());
-    // const data = await xlstojson('data.xlsx', 'data.json');
-    const data = JSON.parse(fs.readFileSync('data.json'));
+    const data = await xlstojson('data.xlsx', 'data.json');
+    // const data = JSON.parse(fs.readFileSync('data.json'));
     const browser = await puppeteer.launch({
-      // headless: false,
-      headless: true,
+      headless: false,
+      // headless: true,
       args: [
         '--window-size=1366,768', 
-        '--no-sandbox',
+        // '--no-sandbox',
       ]
     });
     const page = await browser.newPage();
@@ -41,7 +41,7 @@ const fs = require('fs');
       }),
       page.click('button.log-in'),
     ]);
-    for (let i = 320; i < data.length; i++) {   
+    for (let i = 0; i < data.length; i++) {   
       console.log(i, 'Now of link #: ', i);
       const response = await page.goto(data[i].url, {
         timeout: 0,
